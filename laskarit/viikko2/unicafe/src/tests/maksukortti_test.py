a1520138d85c2134b11c5f4c57264e474084540d
Vesa Rautiainen <veskurau@gmail.com>
2023-03-23 16:21:46 +0200

    Lisätty .gitignore ja maksukortti_test.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_rahan_ottaminen_toimii_kun_tarpeeksi_rahaa(self):
+        totuus = self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+        self.assertEqual(totuus, True)
+
+    def test_rahan_ottaminen_toimii_kun_ei_tarpeeksi_rahaa(self):
+        totuus = self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+        self.assertEqual(totuus, False)
+
+    # def test_rahan_ottaminen_palauttaa_oikean_totuusarvon(self):
+    #     self.maksukortti.ota_rahaa(1100)
+    #     self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
